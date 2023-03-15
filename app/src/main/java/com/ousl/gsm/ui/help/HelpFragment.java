8d7d278582fd690d64298cfa218e3f19e1a5fcf6
ushanchamod <ushanchamodbandara@gmail.com>
2023-03-15 14:43:00 +0530

    Finish help page

--- app/src/main/java/com/ousl/gsm/ui/help/HelpFragment.java
+++ app/src/main/java/com/ousl/gsm/ui/help/HelpFragment.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+
+import com.ousl.gsm.R;
 import com.ousl.gsm.databinding.FragmentHelpBinding;
 
 
@@ -13,19 +18,66 @@ public class HelpFragment extends Fragment {
 
     private FragmentHelpBinding binding;
 
+    // Define btn for toggle
+    private ConstraintLayout faq_1_btn, faq_2_btn;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-
         binding = FragmentHelpBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        return root;
+        return binding.getRoot();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Initialize btn hear
+        faq_1_btn = getView().findViewById(R.id.FAQ_toggle_btn_1);
+        faq_2_btn = getView().findViewById(R.id.FAQ_toggle_btn_2);
+
+        // Define and initialize Discription
+        TextView discription1 = getView().findViewById(R.id.FAQ_one_discription);
+        TextView discription2 = getView().findViewById(R.id.FAQ_two_discription);
+
+
+
+
+
+        // Minimize all descriptions
+        discription1.setVisibility(TextView.GONE);
+        discription2.setVisibility(TextView.GONE);
+
+
+
+
+
+        // Create listners
+        faq_1_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                TextView discription = getView().findViewById(R.id.FAQ_one_discription);
+                discription1.setVisibility(discription1.getVisibility() == TextView.GONE ? TextView.VISIBLE : TextView.GONE);
+            }
+        });
+
+        faq_2_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                TextView discription = getView().findViewById(R.id.FAQ_one_discription);
+                discription2.setVisibility(discription2.getVisibility() == TextView.GONE ? TextView.VISIBLE : TextView.GONE);
+            }
+        });
+    }
+
+
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 }
